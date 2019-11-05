b0f87ade4a12d26bfeae43bc67b091ea387a5d94
Random User <rndusr@posteo.de>
2019-11-05 11:58:43 +0100

    Properly handle broken pipes

--- torfcli/__init__.py
+++ torfcli/__init__.py
@@ -9,31 +9,38 @@
 # GNU General Public License for more details
 # http://www.gnu.org/licenses/gpl-3.0.txt
 
+import sys
+import errno
 
 def run():
     from ._main import run
     from ._errors import MainError
     from ._vars import __appname__
-    import sys
-    import errno
 
     try:
         run()
-
     except MainError as e:
         print(f'{__appname__}: {e}', file=sys.stderr)
         sys.exit(e.errno or 1)
-
     except KeyboardInterrupt:
         print(f'{__appname__}: Aborted', file=sys.stderr)
-
+        sys.exit(errno.ECANCELED)
+    except BrokenPipeError:
+        print(f'{__appname__}: Broken pipe', file=sys.stderr)
+        sys.exit(errno.EPIPE)
+    except BaseException as e:
+        # Because we close stderr ourselves (see below), the Python interpreter
+        # can't report tracebacks anymore (I think)
+        import traceback
+        traceback.print_exc()
+        raise
+    finally:
         # Close stdout and stderr before the python interpreter tries it outside
-        # of our scope, which might fail if a pipe was broken, resuling in an
-        # ugly BrokePipeError message.
+        # of our scope, which might fail if a pipe was broken (e.g. `torf ... |
+        # ls`), resuling in an ugly BrokePipeError message.
         # https://stackoverflow.com/a/18954489
         for fd in (sys.stdout, sys.stderr):
             try:
                 fd.close()
             except IOError:
                 pass
-        sys.exit(errno.ECANCELED)
