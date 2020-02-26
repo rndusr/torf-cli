2a89295f5998871f227a820ec743c9a2e51e5a20
Random User <rndusr@posteo.de>
2020-02-26 16:20:11 +0100

    Indicate support for Python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -32,5 +32,7 @@ setup(
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
     ]
 )
