a67d3b23812b68aa8eade525eabfbffdab4617bc
Random User <rndusr@posteo.de>
2020-04-04 11:11:24 +0200

    setup.py: Indicate support for Python 3.*

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3',
     ]
 )
