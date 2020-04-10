9243aa43a5e1bc7a67e840fd7914c5d77af5625d
Random User <rndusr@posteo.de>
2020-04-10 19:29:26 +0200

    Add tests for empty stdout/stderr

--- tests/test_info.py
+++ tests/test_info.py
@@ -17,6 +17,7 @@ def test_nonexisting_torrent_file(capsys):
     mock_exit.assert_called_once_with(err.Code.READ)
     cap = capsys.readouterr()
     assert cap.err == f'{_vars.__appname__}: {nonexising_path}: No such file or directory\n'
+    assert cap.out == ''
 
 
 def test_insufficient_permissions(capsys, create_torrent):
@@ -27,6 +28,8 @@ def test_insufficient_permissions(capsys, create_torrent):
         mock_exit.assert_called_once_with(err.Code.READ)
         cap = capsys.readouterr()
         assert cap.err == f'{_vars.__appname__}: {torrent_file}: Permission denied\n'
+        assert cap.out == ''
+
 
 def test_magnet(capsys, create_torrent, human_readable, clear_ansi, regex):
     with create_torrent(name='foo') as torrent_file:
@@ -34,11 +37,13 @@ def test_magnet(capsys, create_torrent, human_readable, clear_ansi, regex):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Magnet  magnet:\?xt=urn:btih:[0-9a-z]{40}', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Magnet\tmagnet:\?xt=urn:btih:[0-9a-z]{40}', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_name(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -47,11 +52,13 @@ def test_name(capsys, create_torrent, human_readable, clear_ansi, regex):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Name  foo$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Name\tfoo$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_info_hash(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -60,11 +67,13 @@ def test_info_hash(capsys, create_torrent, human_readable, clear_ansi, regex):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Info Hash  [0-9a-z]{40}$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Info Hash\t[0-9a-z]{40}$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_size(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -73,11 +82,13 @@ def test_size(capsys, create_torrent, human_readable, clear_ansi, regex):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Size  [0-9]+ [KMGT]?i?B$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Size\t[0-9]+$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_piece_size(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -86,11 +97,13 @@ def test_piece_size(capsys, create_torrent, human_readable, clear_ansi, regex):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Piece Size  [0-9]+ [KMGT]?i?B$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Piece Size\t[0-9]+$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_piece_count(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -99,11 +112,13 @@ def test_piece_count(capsys, create_torrent, human_readable, clear_ansi, regex):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Piece Count  [0-9]+$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Piece Count\t[0-9]+$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_single_line_comment(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -112,11 +127,13 @@ def test_single_line_comment(capsys, create_torrent, human_readable, clear_ansi,
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Comment  This is my torrent\.$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Comment\tThis is my torrent\.$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 def test_multiline_comment(capsys, create_torrent, human_readable, clear_ansi, regex):
     comment = 'This is my torrent.\nShare it!'
@@ -126,11 +143,13 @@ def test_multiline_comment(capsys, create_torrent, human_readable, clear_ansi, r
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^(\s*)Comment  This is my torrent\.\n'
                                                 r'\1         Share it!$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Comment\tThis is my torrent\.\tShare it!$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_creation_date(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -140,12 +159,14 @@ def test_creation_date(capsys, create_torrent, human_readable, clear_ansi, regex
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Created  2000-05-10 00:30:45$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             exp_timestamp = int(date.timestamp())
             assert cap.out == regex(rf'^Created\t{exp_timestamp}$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_created_by(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -154,11 +175,13 @@ def test_created_by(capsys, create_torrent, human_readable, clear_ansi, regex):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Created By  foo$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Created By\tfoo$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_private(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -167,22 +190,26 @@ def test_private(capsys, create_torrent, human_readable, clear_ansi, regex):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Private  yes$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Private\tyes$', flags=re.MULTILINE)
+            assert cap.err == ''
 
     with create_torrent(private=False) as torrent_file:
         with human_readable(True):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(r'^\s*Private  no', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             assert cap.out == regex(r'^Private\tno', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_trackers___single_tracker_per_tier(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -193,12 +220,14 @@ def test_trackers___single_tracker_per_tier(capsys, create_torrent, human_readab
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex(rf'^(\s*)Trackers  {trackers[0]}\n'
                                                 rf'\1          {trackers[1]}$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             exp_trackers = '\t'.join(trackers)
             assert cap.out == regex(rf'^Trackers\t{exp_trackers}$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 def test_trackers___multiple_trackers_per_tier(capsys, create_torrent, human_readable, clear_ansi, regex):
     trackers = ['http://tracker1.1',
@@ -212,6 +241,7 @@ def test_trackers___multiple_trackers_per_tier(capsys, create_torrent, human_rea
                                                 rf'\1          Tier 2: http://tracker2.1\n'
                                                 rf'\1                  http://tracker2.2\n'
                                                 rf'\1          Tier 3: http://tracker3.1$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
@@ -219,6 +249,7 @@ def test_trackers___multiple_trackers_per_tier(capsys, create_torrent, human_rea
             exp_trackers = '\t'.join(('http://tracker1.1', 'http://tracker2.1',
                                       'http://tracker2.2', 'http://tracker3.1'))
             assert cap.out == regex(rf'^Trackers\t{exp_trackers}$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_webseeds(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -229,12 +260,14 @@ def test_webseeds(capsys, create_torrent, human_readable, clear_ansi, regex):
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex((rf'^(\s*)Webseeds  {webseeds[0]}\n'
                                                  rf'\1          {webseeds[1]}$'), flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             exp_webseeds = '\t'.join(webseeds)
             assert cap.out == regex(rf'^Webseeds\t{exp_webseeds}$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_httpseeds(capsys, create_torrent, human_readable, clear_ansi, regex):
@@ -245,12 +278,14 @@ def test_httpseeds(capsys, create_torrent, human_readable, clear_ansi, regex):
             cap = capsys.readouterr()
             assert clear_ansi(cap.out) == regex((rf'^(\s*)HTTP Seeds  {httpseeds[0]}\n'
                                                  rf'\1            {httpseeds[1]}$'), flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
             cap = capsys.readouterr()
             exp_httpseeds = '\t'.join(httpseeds)
             assert cap.out == regex(rf'^HTTP Seeds\t{exp_httpseeds}$', flags=re.MULTILINE)
+            assert cap.err == ''
 
 
 def test_file_tree_and_file_count(capsys, create_torrent, human_readable, tmpdir, clear_ansi, regex):
@@ -279,6 +314,7 @@ def test_file_tree_and_file_count(capsys, create_torrent, human_readable, tmpdir
                                                 r'\1             │     └─file3 \[4 B\]\n'
                                                 r'\1             └─subdir2\n'
                                                 r'\1               └─file4 \[4 B\]$', flags=re.MULTILINE)
+            assert cap.err == ''
 
         with human_readable(False):
             run(['-i', torrent_file])
@@ -289,3 +325,4 @@ def test_file_tree_and_file_count(capsys, create_torrent, human_readable, tmpdir
 	                           'root/subdir1/subsubdir1.0/subsubdir1.0.0/file3',
 	                           'root/subdir2/file4'))
             assert cap.out == regex(rf'^Files\t{exp_files}$', flags=re.MULTILINE)
+            assert cap.err == ''
