5d79fde76aad4c3d5ce076c1a9cc554aea1efb5a
Random User <rndusr@posteo.de>
2024-03-24 13:05:04 +0100

    _JSONFormatter.files(): Return list instead of generator

--- torfcli/_ui.py
+++ torfcli/_ui.py
@@ -293,10 +293,10 @@ class _JSONFormatter(_MachineFormatter):
         return torrent.private
 
     def files(self, torrent):
-        return (
+        return [
             {'Path': str(f), 'Size': f.size}
             for f in torrent.files
-        )
+        ]
 
     def info(self, key, value, newline=None):
         # Make sure we can JSON-encode all kinds of iterable
